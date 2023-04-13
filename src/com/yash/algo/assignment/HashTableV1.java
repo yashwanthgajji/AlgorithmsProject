ecc44907a30c32a0a2b37c33f054358f8b3d2efb
Yashwanth Gajji <yashwanth.gajji333@gmail.com>
2023-04-12 21:33:50 -0400

    hashtable v1 added

--- src/com/yash/algo/assignment/HashTableV1.java
+++ src/com/yash/algo/assignment/HashTableV1.java
@@ -1,18 +1,33 @@
 package com.yash.algo.assignment;
 
+import java.util.HashSet;
+
 public class HashTableV1<T extends Comparable<T>> extends DataStructureV1<T> {
+
+    HashSet<T> hashSet;
+
+    HashTableV1() {
+        hashSet = new HashSet<>();
+    }
     @Override
     public void insert(T key) {
-
+        hashSet.add(key);
     }
 
     @Override
     public void delete(T key) throws ElementNotFoundException {
-
+        if (!hashSet.contains(key)) {
+            throw new ElementNotFoundException();
+        } else {
+            hashSet.remove(key);
+        }
     }
 
     @Override
     public void ascendingOrder() {
-
+        for(T i: hashSet) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
